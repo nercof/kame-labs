2ade26cb72fb07b875d53f7def75b05ee1741dea
nercof <gheize@gmail.com>
2017-02-07 14:44:35 +0000

    Add more blog details

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    blog_title = 'KameLabs blog'
+    posts = Post.objects.order_by('published_date')
+    return render(request, 'blog/post_list.html', 
+        {'posts':posts, 'blog_title':blog_title})
